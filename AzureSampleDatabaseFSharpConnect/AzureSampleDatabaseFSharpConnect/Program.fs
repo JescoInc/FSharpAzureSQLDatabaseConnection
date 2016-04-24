ff5a02978f38247ed9d04d0240a0d5a315f975e5
Jesse Glover <silverdrakeproductions@gmail.com>
2016-04-24 17:13:49 -0400

    F Sharp connecting to Microsoft Azure Sample Database and printing the company name and phone number.

---
+++ AzureSampleDatabaseFSharpConnect/AzureSampleDatabaseFSharpConnect/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+open System.Data
+open System.Data.Linq
+open Microsoft.FSharp.Data.TypeProviders
+open Microsoft.FSharp.Linq
+
+type dbSchema = SqlDataConnection<"Data Source=fsharptest.database.windows.net;Initial Catalog=ConnectFSharp;Integrated Security=False;User ID=test;Password=Passw0rd;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False">
+let db = dbSchema.GetDataContext()
+
+let table1 = db.SalesLT_Customer
+
+let query1 =
+        query {
+            for row in db.SalesLT_Customer do
+            select row
+        }
+query1 |> Seq.iter (fun row -> printfn "%s %A" row.CompanyName row.Phone)
+Console.ReadLine()
